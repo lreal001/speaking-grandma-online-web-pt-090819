24ed49d8c40ac8d7c2fc68feb39afb4b5def7b8b
Lessllie Real <rlessllie@yahoo.com>
2019-09-15 02:06:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-string="#{gets}"
+string="#{input.gets}"
 
 def speak_to_grandma()
   if string== "I LOVE YOU GRANDMA!"
