986131d2497beb1e0c54a094d0b822f2f6b06089
Lessllie Real <rlessllie@yahoo.com>
2019-09-15 02:47:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,19 +18,19 @@ string= "Hi"
 string= "WHAT DID YOU EAT TODAY?"
 
 def speak_to_grandma(string)
-  if string== "I LOVE YOU GRANDMA!"
+  if speak_to_grandma== "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
-  if string== "Hi Nana, how are you?"
+  if speak_to_grandma== "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
   end
-  if string== "Hi"
+  if speak_to_grandma== "Hi"
     puts "HUH, SPEAK UP, SONNY!"
   end
-  if string== "WHAT DID YOU EAT TODAY?"
+  if speak_to_grandma== "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
   end
-    if string== "WHAT?" 
+    if speak_to_grandma== "WHAT?" 
     puts "NO, NOT SINCE 1938!"
   end 
   
