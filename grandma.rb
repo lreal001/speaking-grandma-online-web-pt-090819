e42c64ab8523f5a9daeaa78c7e182ef36676efe1
Lessllie Real <rlessllie@yahoo.com>
2019-09-15 02:56:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -24,7 +24,11 @@ def speak_to_grandma(string)
   if string== "Hi Nana, how are you?" || "Hi"
     return "HUH?! SPEAK UP, SONNY!"
   end
-  
+  if string== "Hi"
+    return "HUH, SPEAK UP, SONNY!"
+    
+  end 
+    
   if string== "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
   end
