a9e7bd122f6ce748182591c557ffcb0245587031
Lessllie Real <rlessllie@yahoo.com>
2019-09-15 02:02:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,5 +23,7 @@ def speak_to_grandma()
     puts "HUH, SPEAK UP, SONNY!"
   if string== "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
+    if string== "WHAT?" 
+    puts "NO, NOT SINCE 1938!"
 
 end
